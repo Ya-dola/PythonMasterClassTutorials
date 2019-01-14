0b338b507ad847ab39cece66fe911842455ae369
Ya-dola <Ya-dola@users.noreply.github.com>
2019-01-14 19:54:31 +0300

    Adding If Condition

--- tutorial/test.py
+++ tutorial/test.py
@@ -1,3 +1,8 @@
 print("Testing Print")
 test = input("Who are you? ")
-print(test)
+print("You are: " + test)
+
+# If Function with converting from int to str
+numTest = (3+4)
+if (numTest == 7):
+    print("Correct Answer: " + str(numTest))
