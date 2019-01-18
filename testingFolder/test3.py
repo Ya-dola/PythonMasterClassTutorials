7437ec74cba62368a4ccea538ca6330ee282be1e
Ya-dola <Ya-dola@users.noreply.github.com>
2019-01-18 20:10:08 +0300

    Reading a file

--- testingFolder/test3.py
+++ testingFolder/test3.py
@@ -2,6 +2,10 @@
 
 # Write a file
 file = open("testFile.txt", "w")
-file.write("Test File Written !")
+file.write("Test File Written ! \n2nd line")
 file.close()
 print("File Written !")
+
+# Read a file
+readFile = open("testFile.txt", "r")
+print(readFile.read())
