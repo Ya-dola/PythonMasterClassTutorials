e3fc21d6a6ec83cb9872da2e359ba7e00919fd8a
Ya-dola <Ya-dola@users.noreply.github.com>
2019-02-02 08:05:19 +0300

    Adding Advanced List Slicing

--- testingFolder/test4.py
+++ testingFolder/test4.py
@@ -10,3 +10,13 @@ print(testList[5:])
 
 # Return from start till index
 print(testList[:6])
+
+# Adding a step to Slicing
+print(testList[0:5:2])
+
+# Reverse List using Slicing
+print(testList[::-1])
+
+# Using Slicing in Strings
+testStr = "Elloooo"
+print(testStr[::-1])
