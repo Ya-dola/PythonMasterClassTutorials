e91d43996f0df870aca78cccf5baa8ea76154f9e
Ya-dola <Ya-dola@users.noreply.github.com>
2019-02-02 07:59:38 +0300

    Adding List Slicing

---
+++ testingFolder/test4.py
@@ -0,0 +1,12 @@
+# List Slicing
+
+testList = [6, 9, 2, 1, 69, 21, "ELLOOO", "NO"]
+
+# Return between range
+print(testList[2:4])
+
+# Return from index till end
+print(testList[5:])
+
+# Return from start till index
+print(testList[:6])
