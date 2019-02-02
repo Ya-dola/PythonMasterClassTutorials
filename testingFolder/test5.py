e06e044fee7be411c93d2b8e8058427409102f74
Ya-dola <Ya-dola@users.noreply.github.com>
2019-02-02 09:11:05 +0300

    Adding Maps

--- testingFolder/test5.py
+++ testingFolder/test5.py
@@ -1,2 +1,16 @@
 # Using Lambda functions
 print((lambda x, y: str(x**2) + " " + y)(4, "Ello"))
+
+# Using Map (Recursive way of applying a function to a collection of data (list,set,dictionary) - most probably)
+testSet = {1, 4, 9, 16, 25}
+
+print(testSet)
+
+
+def mapFunc(x):
+    return x + 2
+
+
+testMappedSet = set(map(mapFunc, testSet))
+
+print(testMappedSet)
