6906a47d6680f1fe5712588244fdc0fbc74fdbc2
Ya-dola <Ya-dola@users.noreply.github.com>
2019-02-02 08:08:36 +0300

    Adding Sets

--- testingFolder/test4.py
+++ testingFolder/test4.py
@@ -20,3 +20,8 @@ print(testList[::-1])
 # Using Slicing in Strings
 testStr = "Elloooo"
 print(testStr[::-1])
+
+# Sets - Returns Distinct elements in lists
+testSetList = [1, 1, 1, 1, 2, 2, 2, 5, 5, 5, 3, 8, 2,
+               1, 7, 6, 4, 3, 5, 6, 7, 3, 2, 3, 5, 6, 2, 3, 9]
+print(set(testSetList))
